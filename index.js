0f9b531d37e09cf19e2872602d5809073fb43645
Romendra Uttam <120070407+uttam-on-git@users.noreply.github.com>
2025-01-18 15:10:28 +0530

    initialize notes server with basic CRUD functionality and configuration files

---
+++ index.js
@@ -0,0 +1,72 @@
+import express from "express";
+import cors from 'cors'
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((note) => Number(note.id))) : 0;
+  return String(maxId + 1);
+};
+
+app.post("/api/notes", (req, res) => {
+  const body = req.body;
+  if (!body.content) {
+    return res.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
+  notes = notes.concat(note);
+  res.json(note);
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(400).end();
+  }
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  notes = notes.filter((note) => note.id !== id);
+  res.status(204).end();
+});
+
+const PORT = 3000;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
